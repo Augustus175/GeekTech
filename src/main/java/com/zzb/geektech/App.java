7b6330005df22f07050ef67d626b3e4af70249c2
Augustus175 <columbia7311@163.com>
2019-01-12 22:46:14 +0800

    2019-01-12 22:45:46

--- src/main/java/com/zzb/geektech/App.java
+++ src/main/java/com/zzb/geektech/App.java
@@ -1,13 +1,16 @@
 package com.zzb.geektech;
 
+import java.net.URL;
+import java.util.List;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println(App.class.getClassLoader());
+        URL url = App.class.getClassLoader().getResource("conf/test.txt");
+        System.out.println(App.class.getClassLoader().getResource("conf/test.txt"));
+        System.out.println(App.class.getClassLoader().getResource("/"));
     }
 }
--- src/main/java/com/zzb/geektech/java56/chapter/MyDynamicProxy.java
+++ src/main/java/com/zzb/geektech/java56/chapter/MyDynamicProxy.java
@@ -1,6 +1,51 @@
 package com.zzb.geektech.java56.chapter;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+interface Hello {
+    void sayHello();
+}
+
+class HelloImpl implements Hello {
+    @Override
+    public void sayHello() {
+        System.out.println("Hello! ");
+    }
+}
+
+class MyInvocationHandler implements InvocationHandler {
+
+    private Object target;
+
+    public MyInvocationHandler(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) {
+        System.out.println("Invoking sayHello");
+        Object result = null;
+        try {
+            result = method.invoke(target, args);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return result;
+
+    }
+}
+
 public class MyDynamicProxy {
     public static void main(String[] args) {
+        Hello hello = new HelloImpl();
+        MyInvocationHandler handler = new MyInvocationHandler(hello);
+        Hello proxyHello = (Hello) Proxy.newProxyInstance(Hello.class.getClassLoader()
+                , HelloImpl.class.getInterfaces(), handler);
+        proxyHello.sayHello();
     }
 }
